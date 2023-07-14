25ac66118e4989ba84f0ec970987a11b1ad766f3
jieun59 <jieunyun33@gmail.com>
2023-07-14 17:19:39 +0900

    첫번째 작업 파일 thebrains27

---
+++ Test/src/main/java/com/dustmg/controller/BookController.java
@@ -0,0 +1,10 @@
+package com.dustmg.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class BookController {
+	@RequestMapping("bookList.do")
+	public void bookList() {
+		
+	}
+}
